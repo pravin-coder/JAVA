d5be4330a163bf0f72936e5d81b6b9b3021803ea
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2024-03-15 08:38:50 +0530

    Create prog18.java

---
+++ Day2/prog18.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DateTimeExample {
+    public static void main(String[] args) {
+        // Creating a Date object with the current date and time
+        Date currentDate = new Date();
+
+        // Displaying the current date and time
+        System.out.println("Current Date and Time: " + currentDate);
+    }
+}
