0fce70c59f8f2d551bbf109782bae1ae0e49c92a
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2024-03-20 14:00:05 +0530

    Create prog8.java

---
+++ Day-4/prog8.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NthLargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the value of N: ");
+        int N = scanner.nextInt();
+
+        int nthLargest = findNthLargest(array, N);
+
+        System.out.println("The " + N + "th largest number in the array is: " + nthLargest);
+    }
+
+    public static int findNthLargest(int[] array, int N) {
+        Arrays.sort(array); // Sort the array in ascending order
+        int index = array.length - N; // Index of the Nth largest element
+        return array[index];
+    }
+}
